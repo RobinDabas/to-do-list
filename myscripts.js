4b425adb1f5bcc229446b339ad9b4998f81c6003
ROBERTO D <robindabas@hotmail.com>
2025-04-30 19:37:30 -0400

    javascript practice

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,34 @@
-document.addEventListner('DOMContentLoaded', () => {
-  const taskInput = document.getElementById('taskInput');
-  const addButton = document.getElementById("addTaskBtn");
-  const taskList = document.querySelector('.taskList');
+var tribe;
+var tribe = "Banu Gadhadfa";
+console.log(tribe);
 
-  function addTask() {
-    const taskText = taskInput.value.trim();
-    if (taskText !== '') {
-      const listItem = document.createElement('li');
-      listItem.textContent = taskText;
-    }
-  }
-});
+console.log("Ya hala, ya hala " + tribe + " min Libiyyah")
+
+var tribe = "Qahtan 505 tribe";
+var region = "Barqa"
+
+console.log("The " + tribe + " is located in the region of " + region + " in the Arab country of Libya.");
+
+
+let Maghreb = "Arabian FGC-1 mutation";
+let Amazigh = "fabricated";
+let Berber = Amazigh;
+
+
+let Libya = "Most Arabian country outside of the Arabian peninsula";
+if (Libya != Amazigh) {
+  console.log("Libyans are direct descendants of the Arabian Peninsula.")
+}
+
+let areNotAmazigh = (Libya != Amazigh);
+console.log(areNotAmazigh);
+
+console.log(515 === "515");
+
+
+let laHoraActual = 10;
+console.log(laHoraActual > 9 && laHoraActual < 17);
+
+var northAfrica = true;
+
+console.log ("North Africa is Amazigh:" +  !northAfrica);
