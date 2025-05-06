269d81877b3b4dfd8c89c5ec2327a69f32e67992
ROBERTO D <robindabas@hotmail.com>
2025-05-06 16:34:48 -0400

    operators practice

--- myscripts.js
+++ myscripts.js
@@ -32,3 +32,54 @@ console.log(laHoraActual > 9 && laHoraActual < 17);
 var northAfrica = true;
 
 console.log ("North Africa is Amazigh:" +  !northAfrica);
+
+
+console.log("Libya " + "Qahtan " + "505");
+
+console.log((5 * 10) + '5');
+
+var qahtan = 1000000; //Population of the Qahtan tribe in the Arab world//
+qahtan += 50000; //Libya//
+qahtan += 10000000; //Yemen//
+qahtan += 800000; //Mauritania//
+qahtan += 75000; //Sahrawi//
+
+console.log(qahtan);
+
+//The concatenation assignment operator, +=//
+
+var tribe = "";
+tribe += "Ana";
+tribe += " min";
+tribe += " qabilat Jabbour";
+tribe += " Madh'hij";
+tribe += " Al-Ta'an";
+tribe += " Qahtan 505";
+tribe += " min madina Ajdabiya";
+tribe += " fi Libya";
+
+console.log(tribe);
+
+
+console.log(1005 - 500 * 6);
+
+
+
+var score = 8;
+console.log("Mid-level skills:" + (score > 0 && score < 10));
+
+
+var timeRemaining = 0;
+var energy = 10;
+console.log('Game over: ' + (timeRemaining == 5 || energy == 5));
+
+var num1 = 2;
+var num2 = 5;
+var test1 = (num1 % 2);
+var test2 = (num2 % 2);
+var result1 = (test1 == 0);
+var result2 =(test2 == 0);
+
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
+
